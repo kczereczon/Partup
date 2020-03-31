e4508177c04eaa5b25c0b83d7998d9279787be23
piotranon <piotranon.1@gmail.com>
2020-03-31 15:02:14 +0200

    user_course many to many relationship

--- app/Course.php
+++ app/Course.php
@@ -8,6 +8,11 @@ class Course extends Model
 {
     protected $fillable = ['name', 'group_id'];
 
+    public function users()
+    {
+        return $this->belongsToMany(Users::class);
+    }
+
     public function group()
     {
         return $this->belongsTo(Group::class);
