7b533cb040f297192efca5db881cc52362b3b81b
kczereczon <kczereczon@gmail.com>
2020-04-05 14:23:13 +0200

    Api config changes.

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.APP_URL,
+    baseURL: "/api",
     headers: {
         Authorization: "Bearer " + window.localStorage.getItem("authToken"),
     },
