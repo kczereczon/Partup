db94c086a27ea08cb5b854c672e2c61657137614
kczereczon <kczereczon@gmail.com>
2020-04-05 14:04:04 +0200

    changes in API config

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://partup.herokuapp.com/api",
+    baseURL: process.env.APP_URL,
     headers: {
         Authorization: "Bearer " + window.localStorage.getItem("authToken"),
     },
