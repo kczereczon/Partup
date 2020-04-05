e1fb1df07efae078cf8cffffb759ab3e7961922b
kczereczon <kczereczon@gmail.com>
2020-04-05 16:11:12 +0200

    Fixing api config

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.APP_URL
-        ? process.env.APP_URL + "/api"
-        : "http://localhost:8000/api",
+    baseURL: "/api",
     headers: {
         Authorization: "Bearer " + window.localStorage.getItem("authToken"),
     },
